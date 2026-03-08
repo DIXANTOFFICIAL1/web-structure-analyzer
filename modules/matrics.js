2680a25cb7d1a80cbebc34cd52c77e9f16947680
DIXANT SONI <dixantofficial@gmail.com>
2026-03-08 21:48:42 +0530

    Update matrics.js

--- modules/matrics.js
+++ modules/matrics.js
@@ -1,1 +1,26 @@
+export function calculateMetrics(content){
 
+const lines = content.split("\n")
+const words = content.split(/\s+/).length
+const characters = content.length
+
+const tagMatches = content.match(/<(\w+)/g) || []
+
+const tagCounts = {}
+
+tagMatches.forEach(tag=>{
+const clean = tag.replace("<","").toLowerCase()
+tagCounts[clean] = (tagCounts[clean] || 0) + 1
+})
+
+const domNodes = tagMatches.length
+
+return{
+lines: lines.length,
+words,
+characters,
+tagCounts,
+domNodes
+}
+
+}
