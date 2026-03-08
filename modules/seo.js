3d712a945a171ef4f5b0fb30c24a51286e321a36
DIXANT SONI <dixantofficial@gmail.com>
2026-03-08 21:45:49 +0530

    Update seo.js

--- modules/seo.js
+++ modules/seo.js
@@ -1,12 +1,9 @@
 export function analyzeSEO(content){
 
 const hasTitle = /<title>.*<\/title>/i.test(content)
-
 const hasMetaDesc = /<meta\s+name="description"/i.test(content)
 
 return{
 hasTitle,
 hasMetaDesc
-}
-
-}
+}}
