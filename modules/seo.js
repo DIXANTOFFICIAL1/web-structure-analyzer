2ba4b186e1def7474465b07f709d8eb6613864f5
DIXANT SONI <dixantofficial@gmail.com>
2026-03-08 21:45:12 +0530

    Create seo.js

---
+++ modules/seo.js
@@ -0,0 +1,12 @@
+export function analyzeSEO(content){
+
+const hasTitle = /<title>.*<\/title>/i.test(content)
+
+const hasMetaDesc = /<meta\s+name="description"/i.test(content)
+
+return{
+hasTitle,
+hasMetaDesc
+}
+
+}
