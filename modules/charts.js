3db6b864e417bfdb16d4878acfe051c6280af53b
DIXANT SONI <dixantofficial@gmail.com>
2026-03-09 21:47:29 +0530

    Update charts.js

--- modules/charts.js
+++ modules/charts.js
@@ -1,1 +1,16 @@
+export function renderChart(tagCounts){
 
+const ctx = document.getElementById("tagChart")
+
+new Chart(ctx,{
+type:"bar",
+data:{
+labels:Object.keys(tagCounts),
+datasets:[{
+label:"Tag Frequency",
+data:Object.values(tagCounts)
+}]
+}
+})
+
+}
