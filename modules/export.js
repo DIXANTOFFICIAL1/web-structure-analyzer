5dd79f1f329b8334aa1a4a1b1022093bd1ec822a
DIXANT SONI <dixantofficial@gmail.com>
2026-03-13 23:44:05 +0530

    Update export.js

--- modules/export.js
+++ modules/export.js
@@ -1,1 +1,13 @@
+export function exportReport(data){
 
+const blob = new Blob(
+[JSON.stringify(data,null,2)],
+{type:"application/json"}
+)
+
+const a = document.createElement("a")
+a.href = URL.createObjectURL(blob)
+a.download = "html-analysis-report.json"
+a.click()
+
+}
