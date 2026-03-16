14313843e016aa5a9f0b431a2a404c818c305412
DIXANT SONI <dixantofficial@gmail.com>
2026-03-16 23:51:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ let analysisResult={}
 themeToggle.onclick=()=>{
 document.body.classList.toggle("dark")
 }
-
 dropArea.addEventListener("dragover",e=>{
 e.preventDefault()
 dropArea.style.background="#e0e7ff"
